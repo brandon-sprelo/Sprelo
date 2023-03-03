a424300c43bdd43b9ea8541bd97c5befb870fe6e
William Yang <williamyang93@gmail.com>
2023-03-03 15:45:31 -0800

    Set up chakra ui. Set up pages for landing login sign up

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Login() {
+
+  return (
+    <>
+    <Head>
+      <title>Sprelo - Log In</title>
+    </Head>
+    <div>Login</div>
+    </>
+  )
+}
