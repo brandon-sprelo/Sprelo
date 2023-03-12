d4deb254e3136cdce4261c3244619d333f956158
William Yang <williamyang93@gmail.com>
2023-03-11 20:02:49 -0800

    Added google script to next js

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,21 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/SpreloLogoOnly.svg" />
+        {/* Google Analytics tracking code */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5M2Y1FQ3F0"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5M2Y1FQ3F0');
+              `,
+          }}
+        ></script>
       </Head>
       <body>
         <Main />
