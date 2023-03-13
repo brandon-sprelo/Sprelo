503e2cb55f54efc48f861ad72bb521d9ddb5b579
William Yang <williamyang93@gmail.com>
2023-03-13 13:55:40 -0700

    Updated config for github pages and added deploy to package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/login": { page: "/login" },
+      "/signup": { page: "/signup" },
+      // "/posts": { page: "/posts" },
+      // "/posts/first-post": { page: "/posts/[id]", query: { id: "first-post" } },
+      // "/posts/second-post": {
+      //   page: "/posts/[id]",
+      //   query: { id: "second-post" },
+      // },
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
